d88f3bc0176771f3ec36a8773cec279e82895c19
hurynovich <pavel.hurynovich@gmail.com>
2020-02-27 22:35:26 +0300

    added few more logging lines

--- tinylog/src/main/java/io/github/hurynovich/log/tinylog/Main.java
+++ tinylog/src/main/java/io/github/hurynovich/log/tinylog/Main.java
@@ -3,11 +3,15 @@ package io.github.hurynovich.log.tinylog;
 import static org.tinylog.Logger.*;
 
 public class Main {
-
     public static void main(String[] args) {
         debug("Hi from debug.");
         info("Hi this is info.");
-        error("Oops", new Exception("!"));
+        warn("Here is a warning.");
+        error(new Exception("Some problem."), "Oops, we have a problem.");
+        error(new Exception("Just exception."));
+        error("Just message.");
 
+        info("Simple placeholder: '{}'.", ":)");
+        info("Lambdas as parameter: '{} rolls {}'.", () -> System.getProperty("user.name"), Math::random );
     }
 }
